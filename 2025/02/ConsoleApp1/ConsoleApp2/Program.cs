d21f32090b234b2b11c85379892482bf5a77dcab
pejoedev <jondevpet@gmail.com>
2025-12-02 11:13:30 +0100

    puzzle 2 wrks with sample

--- 2025/02/ConsoleApp1/ConsoleApp2/Program.cs
+++ 2025/02/ConsoleApp1/ConsoleApp2/Program.cs
@@ -14,16 +14,36 @@ foreach (string range in ranges)
     // Console.WriteLine(key1 + " != " + key2);
     for (int i = 0; i < key2 - key1 + 1; i++)
     {
-        int lenght = $"{key1 + i}".Length;
-        if (lenght % 2 != 0) continue;
-        // Console.WriteLine(key1 + i);
-        string firstHalf = $"{key1 + i}".Substring(0,lenght / 2);
-        string secondHalf = $"{key1 + i}".Substring(lenght / 2,lenght / 2);
-        if (firstHalf == secondHalf)
+        // checking each key
+        int lengthOfKey = $"{key1 + i}".Length;
+        for (int j = 1; j < lengthOfKey + 1; j++)
         {
-            Console.WriteLine($"{firstHalf} + {secondHalf} = {firstHalf + secondHalf}");
-            invalidTotal += (key1 + i);
+            // checking the key at different scanning sizes
+            if (lengthOfKey % j != 0 || lengthOfKey == j) continue;
+            // Console.WriteLine("Checking for a length of " + j);
+            string firstHalf = $"{key1 + i}".Substring(0,j);
+            bool isValid = true;
+            for (int k = 1; k < (lengthOfKey / j); k++)
+            {
+                // checking the key at each index with a certain length
+                if (!isValid) continue;
+                if ($"{key1 + i}".Substring(k * j, j) != firstHalf)
+                {
+                    isValid = false;
+                    // Console.WriteLine($"Failed at {key1 + i} with {firstHalf} as key, {$"{key1 + i}".Substring(k, j)} != '{firstHalf}'");
+                    break;
+                    // try a longer num
+                }
+                // Console.WriteLine("Need to check this again at index " + (k * j));
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine($"Found: {key1 + i}");
+                invalidTotal += key1 + i;
+                break;
+            }
         }
     }
 }
-Console.WriteLine("Total:  " + invalidTotal);
+Console.WriteLine("Total: " + invalidTotal);

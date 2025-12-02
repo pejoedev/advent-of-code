d4ab234dae30cc213f56e1b65911d1328bef4349
pejoedev <jondevpet@gmail.com>
2025-12-02 12:29:39 +0100

    day 1 works on sample dataset

--- 2025/01/ConsoleApp1/ConsoleApp1/Program.cs
+++ 2025/01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO;
 
-string content = File.ReadAllText("../../../input.txt");
-Console.WriteLine(content);
-Console.WriteLine("Hello, World!");
+long index = 50;
+long amountOfZeros = 0;
+foreach (string line in File.ReadLines("../../../input.txt"))
+{
+    Console.WriteLine(line);
+    string character = line.Substring(0,1);
+    string amountString = line.Substring(1,line.Length -1);
+    long amount = Int64.Parse(amountString);
+    if (character == "L")
+    {
+        index -= amount;
+    }
+    else
+    {
+        index += amount;
+    }
+    index = index % 100;
+    if (index < 0)
+    {
+        index += 100;
+    }
+    if (index >= 100)
+    {
+        index -= 100;
+    }
+    
+    if (index == 0)
+    {
+        amountOfZeros++;
+    }
+    Console.WriteLine("Index: " + index + ", Amount: " + amountOfZeros);
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # advent-of-code
 This is my attempt at [advent of code](https://adventofcode.com/).
 
-I am starting with 2025.
+I am starting with 2025. You can also view my progress on [my website](https://pejoe.dev/advent-of-code/) (for now, use internet archive if it doesn't exist anymore xD)

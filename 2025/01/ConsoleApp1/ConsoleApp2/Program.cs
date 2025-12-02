84d887565b26e88a57f42f27dcf03b8ace3f7bce
pejoedev <jondevpet@gmail.com>
2025-12-02 14:34:50 +0100

    WIP

--- 2025/01/ConsoleApp1/ConsoleApp2/Program.cs
+++ 2025/01/ConsoleApp1/ConsoleApp2/Program.cs
@@ -5,15 +5,19 @@ long index = 50;
 long amountOfZeros = 0;
 foreach (string line in File.ReadLines("../../../input.txt"))
 {
-    Console.WriteLine(line);
+    long lastIndex = index;
     string character = line.Substring(0,1);
     string amountString = line.Substring(1,line.Length -1);
     long amount = Int64.Parse(amountString);
+    if (amount == 0)
+    {
+        continue;
+    }
     if (character == "L")
     {
         index -= amount;
     }
-    else
+    else if (character == "R")
     {
         index += amount;
     }
@@ -26,12 +30,29 @@ foreach (string line in File.ReadLines("../../../input.txt"))
     while (index < 0)
     {
         index += 100;
-        amountOfZeros++;
+        if (lastIndex != 0)
+        {
+            amountOfZeros++;
+        }
+    }
+
+    if (index == 100)
+    {
+        index = 0;
     }
     
     if (index == 0)
     {
         amountOfZeros++;
     }
-    Console.WriteLine("Index: " + index + ", Amount: " + amountOfZeros);
+    if (character == "L")
+    {
+        Console.WriteLine("Last Index: " + lastIndex + ", Line: " + line + ", Index: " + index + 
+                          ", Amount: " + amountOfZeros + ", calc: " + lastIndex + " - " + amount + " = " + (lastIndex - amount) + (lastIndex - amount <= 0 ? ", upgrade = true": ""));
+    }
+    else if (character == "R")
+    {
+        Console.WriteLine("Last Index: " + lastIndex + ", Line: " + line + ", Index: " + index + 
+                          ", Amount: " + amountOfZeros + ", calc: " + lastIndex + " + " + amount + " = " + (lastIndex + amount) + (lastIndex + amount >= 100 ? ", upgrade = true": ""));
+    }
 }

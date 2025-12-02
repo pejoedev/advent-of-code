4e4f5cbdd48a3f66f070408cae45883beb056d17
pejoedev <jondevpet@gmail.com>
2025-12-02 12:14:01 +0100

    init day 1

---
+++ 2025/01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
+string content = File.ReadAllText("../../../input.txt");
+Console.WriteLine(content);
+Console.WriteLine("Hello, World!");

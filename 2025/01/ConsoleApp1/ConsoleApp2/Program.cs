78dd59f96091843dd12ee74d4f78be19f2f53102
pejoedev <jondevpet@gmail.com>
2025-12-02 12:35:40 +0100

    day 1 part 2 works with the sample dataset

--- 2025/01/ConsoleApp1/ConsoleApp2/Program.cs
+++ 2025/01/ConsoleApp1/ConsoleApp2/Program.cs
@@ -1,6 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO;
 
-string content = File.ReadAllText("../../../input.txt");
-Console.WriteLine(content);
-Console.WriteLine("Hello, World!");
+long index = 50;
+long amountOfZeros = 0;
+foreach (string line in File.ReadLines("../../../input.txt"))
+{
+    Console.WriteLine(line);
+    string character = line.Substring(0,1);
+    string amountString = line.Substring(1,line.Length -1);
+    long amount = Int64.Parse(amountString);
+    if (character == "L")
+    {
+        index -= amount;
+    }
+    else
+    {
+        index += amount;
+    }
+
+    while (index > 100)
+    {
+        index -= 100;
+        amountOfZeros++;
+    }
+    while (index < 0)
+    {
+        index += 100;
+        amountOfZeros++;
+    }
+    
+    if (index == 0)
+    {
+        amountOfZeros++;
+    }
+    Console.WriteLine("Index: " + index + ", Amount: " + amountOfZeros);
+}
